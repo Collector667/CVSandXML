211c82fbc9ebae6ac821f060f5eb73dd01f44abd
p9trusha <petrnevolin2005@gmail.com>
2024-11-24 15:18:55 +0300

    input

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,25 @@
-import com.opencsv.exceptions.CsvValidationException;
-
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.Reader;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.HashMap;
+import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) throws IOException, CsvValidationException {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        String path = "address.csv";
-        HashMap<String, Csv> addresses;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String combToExit = "exit";
+        String input = null;
+         do {
+             if (input != null) {;
+                 if (input.substring(input.lastIndexOf('.') + 1).equals("csv")) {
+                     HashMap<String, Csv> addresses;
+                     addresses = parseCSV.parse(input);
+                     System.out.println(addresses);
+                 }
+                 else if ((input.substring(input.lastIndexOf('.') + 1).equals("xml"))) {
 
-        addresses = parseCSV.parse(path);
-        System.out.println(addresses);
-
-
+                 }
+             }
+             System.out.printf("Введите путь до файла или %s для завершения работы\n", combToExit);
+             input = sc.nextLine();
+        } while (!input.equals(combToExit));
+        sc.close();
     }
 }

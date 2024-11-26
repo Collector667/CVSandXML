6ed5bc6f7a7f2de1791b1648ae10e5dfe1d5117d
Collector667 <146832679+Collector667@users.noreply.github.com>
2024-11-26 16:23:32 +0300

    .

--- src/Main.java
+++ src/Main.java
@@ -4,28 +4,46 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Objects;
 import java.util.Scanner;
 
 class Processing {
-    HashMap<String, HashMap<HashMap<String, String>, Integer>> directory;
+    HashMap<String,Csv> directory;
 
-    Processing(HashMap<String, HashMap<HashMap<String, String>, Integer>> directory) {
+    Processing(HashMap<String, Csv> directory) {
         this.directory = directory;
     }
 
     void outputDuplicates() {
-        HashMap<HashMap<String, String>, Integer> addressesOfCity;
-        HashMap<String, String> addressWithCity;
+
+
         for (String city : this.directory.keySet()) {
-            addressesOfCity = this.directory.get(city);
-            for (HashMap<String, String> address : addressesOfCity .keySet()) {
-                if (addressesOfCity.get(address) > 1) {
-                    addressWithCity = (HashMap<String, String>) address.clone();
-                    addressWithCity.put("city", city);
-                    System.out.printf("Запись %s повторяется %d раз(а)\n",
-                            addressWithCity, addressesOfCity.get(address));
+            int Size = this.directory.get(city).addressList.size();
+            for (int i = 0; i < Size-1; i++) {
+                while (Objects.equals(this.directory.get(city).addressList.get(i).street, this.directory.get(city).addressList.get(i + 1).street)) {
+                    this.directory.get(city).addressList.remove(i+1);
+                    this.directory.get(city).addressList.get(i).count += 1;
+                    Size = Size - 1;
+                }
+                if (this.directory.get(city).addressList.get(i).count != 1) {
+                    System.out.println(city + " " +this.directory.get(city).addressList.get(i).street + " " + this.directory.get(city).addressList.get(i).count);
                 }
             }
+        }
+    }
+    void countOfhouse() {
+
+        for (String city : this.directory.keySet()) {
+            int[] countFloor = {0, 0, 0, 0, 0};
+            int Size = this.directory.get(city).addressList.size();
+            int k = 0;
+            for (int i = 0; i < Size; i++) {
+                k = Integer.parseInt(this.directory.get(city).addressList.get(i).floor);
+                countFloor[k-1] += 1;
+            }
+            System.out.println(city + " 1 этаж: " + countFloor[0]
+             + " 2 этажа: " + countFloor[1] + " 3 этажа " + countFloor[2] + " 4 этажа: " + countFloor[3] + " 5 этажей: " + countFloor[4]);
+            countFloor[0] = 0;
         }
     }
 }
@@ -46,15 +64,16 @@ public class Main {
                     System.out.println("Файл не найден");
                 }
                 else if (typeOfFile.equals("csv") || typeOfFile.equals("xml")) {
+                    HashMap<String, Csv> addresses = null;
                     if (typeOfFile.equals("csv")) {
-                        HashMap<String, Csv> addresses;
                         addresses = parseCSV.parse(input);
-                        System.out.println(addresses);
+
                     }
-                    else {
-                        directory = parseXML.parse(input);
-                    }
-                    new Processing(directory).outputDuplicates();
+//                    else {
+//                        //directory = parseXML.parse(input);
+//                    }
+                    new Processing(addresses).outputDuplicates();
+                    new Processing(addresses).countOfhouse();
                     endTime = new Date();
                     System.out.printf("Время работы: %d мc\n", (endTime.getTime() - startTime.getTime()));
                 }
--- src/parseCSV.java
+++ src/parseCSV.java
@@ -1,11 +1,49 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
+
 import com.opencsv.CSVReader;
+//class Student implements Comparable<Student> {
+//    private final String name;
+//    private final double avgMark;
+//
+//    public Student(String name, double avgMark) {
+//        this.name = name;
+//        this.avgMark = avgMark;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "{n='" + name + '\'' + ", m=" + avgMark + '}';
+//    }
+//
+//    @Override
+//    public int compareTo(Student o) {
+//        return name.compareTo(o.name);
+//    }
+//}
+class Address1 implements Comparator<Address1> {
+    public String street, floor;
+    int count = 1;
+    public Address1(String street, String floor) {
+    }
+    public String getStreet() {
+        return street;
+    }
+
+    @Override
+    public int compare(Address1 o1, Address1 o2) {
+        return 0;
+    }
+}
+class ComparatorByAvgMark implements Comparator<Address1> {
+    @Override
+    public int compare(Address1 o1, Address1 o2) {
+        return CharSequence.compare(o1.getStreet(), o2.getStreet());
+    }
+}
 
 class Csv {
-    List<Address> addressList = new ArrayList<>();
+    List<Address1> addressList = new ArrayList<>();
 }
 
 public class parseCSV {
@@ -16,13 +54,16 @@ public class parseCSV {
             String[] nextRecord;
             String[] a = new String[0];
             HashMap<String, Csv> addresses = new HashMap<>();
-
+            nextRecord = csvReader.readNext();
             while ((nextRecord = csvReader.readNext()) != null) {
-                Address address = null;
+                Address1 address = null;
                 Csv csv = null;
                 for (String cell : nextRecord) {
                     a = cell.split(";");
-                    address = new Address(a[1], a[2], a[3]);
+                    address = new Address1(a[1], a[3]);
+                    address.street = a[1] + a[2];
+                    address.floor = a[3];
+
                 }
                 csv = new Csv();
                 csv.addressList.add(address);
@@ -32,6 +73,10 @@ public class parseCSV {
                     addresses.get(a[0]).addressList.add(address);
                 }
             }
+            for (String key : addresses.keySet()) {
+                addresses.get(key).addressList.sort(new ComparatorByAvgMark());
+            }
+
             return addresses;
         }
         catch (Exception e) {
